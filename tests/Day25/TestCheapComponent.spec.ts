cec53ad72a5a9072be3c50ebbdd7c3c0eb0cafa7
Diep Nguyen <diepniit.nguyenhong@gmail.com>
2024-06-24 21:46:55 +0700

    remove test.describe

--- tests/Day25/TestCheapComponent.spec.ts
+++ tests/Day25/TestCheapComponent.spec.ts
@@ -6,22 +6,20 @@ import cheapComputerDataList from "../../test-data/CheapComputer.json" assert { 
 import OrderTestFlow from "../../test-flows/computer/OrderTestFlow.js";
 import { getCreditCardNumber } from "../../utils/GetCreditCardNumber.js";
 
-test.describe('Test Cheap Component', () => {
-    test(`${TAG.smoke} | Test Cheap Component`, async ({ page }) => {
-        await page.goto("/build-your-cheap-own-computer")
-        const orderTestFlow: OrderTestFlow = new OrderTestFlow(page, CheapComputerComponent, undefined, cheapComputerDataList)
-        await orderTestFlow.buildComputerDetailListAndAddToCart()
-        await orderTestFlow.navigateToShoppingCartPage()
-        await orderTestFlow.verifyShoppingCart()
-        await orderTestFlow.selectTOSandCheckout()
-        await orderTestFlow.inputBillingAddress()
-        await orderTestFlow.inputShippingAddress()
-        await orderTestFlow.selectShippingMethod()
-        await orderTestFlow.selectPaymentMethod(paymentMethods.creditCard)
-        await orderTestFlow.inputPaymentInfo(await getCreditCardNumber('Mastercard'))
-        await orderTestFlow.confirmOrder()
-        await orderTestFlow.checkoutCompleted()
-        
-        await page.waitForTimeout(2 * 1000)
-    });
-} )
+test(`${TAG.smoke} | Test Cheap Component`, async ({ page }) => {
+    await page.goto("/build-your-cheap-own-computer")
+    const orderTestFlow: OrderTestFlow = new OrderTestFlow(page, CheapComputerComponent, undefined, cheapComputerDataList)
+    await orderTestFlow.buildComputerDetailListAndAddToCart()
+    await orderTestFlow.navigateToShoppingCartPage()
+    await orderTestFlow.verifyShoppingCart()
+    await orderTestFlow.selectTOSandCheckout()
+    await orderTestFlow.inputBillingAddress()
+    await orderTestFlow.inputShippingAddress()
+    await orderTestFlow.selectShippingMethod()
+    await orderTestFlow.selectPaymentMethod(paymentMethods.creditCard)
+    await orderTestFlow.inputPaymentInfo(await getCreditCardNumber('Mastercard'))
+    await orderTestFlow.confirmOrder()
+    await orderTestFlow.checkoutCompleted()
+
+    await page.waitForTimeout(2 * 1000)
+});
