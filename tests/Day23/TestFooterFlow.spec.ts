e58dc0974dce950f5f32a8a9b9aae3335fb473cd
Diep Nguyen <diep.nguyen@itech.asia>
2026-07-09 10:15:21 +0700

    update package.json

--- tests/Day23/TestFooterFlow.spec.ts
+++ tests/Day23/TestFooterFlow.spec.ts
@@ -1,10 +1,9 @@
 import { test } from '@playwright/test'
-import FooterTestFlow from '../../test-flows/global/footerTestFlow'
+import FooterTestFlow from '../../test-flows/global/FooterTestFlow.js'
 
 test('Test Footer Flow', async ({ page }) => {
     await page.goto("https://demowebshop.tricentis.com/")
     let footerTestFlow: FooterTestFlow = new FooterTestFlow(page)
-    await footerTestFlow.verifyFooterComp()
-    
+    await footerTestFlow.verifyFooterComp('')
     await page.waitForTimeout(2 * 1000)
 })
