2fb613531bd336122158af6a07cb4a5ad17f901e
Diep Nguyen <diep.nguyen@itech.asia>
2026-07-14 10:34:30 +0700

    fix: adding retry for CI

--- playwright.config.js
+++ playwright.config.js
@@ -22,10 +22,10 @@ export default defineConfig({
         ['html'],
         ['allure-playwright']
     ],
-    // retries: process.env.CI ? 2 : 1,
+    retries: process.env.CI ? 1 : 0,
     use: {
         baseURL: 'https://demowebshop.tricentis.com',
-        //viewport: { width: 1920, height: 1080 },
+        viewport: { width: 1920, height: 1080 },
         actionTimeout: 5 * 1000,
         trace: 'on-first-retry',
         video: 'on-first-retry',
